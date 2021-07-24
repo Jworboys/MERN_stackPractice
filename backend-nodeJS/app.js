5dd2c086d21a49c1b8bc99ab7bede6e00b653b9c
jworboys <Jordworb@gmail.com>
2021-07-24 00:24:41 -0400

    Impementing some error handeling for the 2 requests. An making an error route for when errors are thrown.

--- backend-nodeJS/app.js
+++ backend-nodeJS/app.js
@@ -5,6 +5,15 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placesRoutes);
+// This filter makes it so it doesnt have to be exact but has to atleast start with /api/places...that is because it app.use rather then a get or post... get or post etc.. makes it so it have to be exact.
+app.use('/api/places', placesRoutes); // => /api/places/...
+app.use((error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An unknown has occured.' });
+});
 
 app.listen(5000);
--- backend-nodeJS/routes/places-routes.js
+++ backend-nodeJS/routes/places-routes.js
@@ -2,10 +2,53 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (request, response, next) => {
-	console.log('GET Request in Places.');
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Empire State Building',
+		description: 'One of the most famous sky scrappers in the world.',
+		location: {
+			lat: 40.7484474,
+			lng: -73.9871516,
+		},
+		address: '20 W 34th St, New York, NY 10001',
+		creator: 'u1',
+	},
+];
+
+router.get('/:pid', (request, response, next) => {
+	const placeId = request.params.pid;
+	// { pid: 'p1' } -- params is an object in which were we can the values from the keys to the desired inputs. -- such as the /:pid in this case.
+
+	const place = DUMMY_PLACES.find(p => {
+		return p.id === placeId;
+	});
+
+	if (!place) {
+		const error = new Error('Could not find a place for the provided ID.');
+		error.code = 404;
+		return next(error); // - Make sure to return our the following code block will still run and will try and return another object.
+	}
+
 	response.json({
-		message: 'It works!',
+		place,
+	});
+});
+
+router.get('/user/:uid', (request, response, next) => {
+	const userId = request.params.uid;
+	const place = DUMMY_PLACES.find(p => {
+		return p.creator === userId;
+	});
+
+	if (!place) {
+		const error = new Error('Could not find a place for the provided user ID.');
+		error.code = 404;
+		return next(error);
+	}
+
+	response.json({
+		place,
 	});
 });
 
