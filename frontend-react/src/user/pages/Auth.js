7479636c14f9c36dcc8ffc0776bc529abf9f3382
jworboys <Jordworb@gmail.com>
2021-07-25 23:34:34 -0400

    Working on connecting front and back end

--- frontend-react/src/user/pages/Auth.js
+++ frontend-react/src/user/pages/Auth.js
@@ -28,9 +28,31 @@ const Auth = () => {
 		false
 	);
 
-	const authSubmitHandler = event => {
+	const authSubmitHandler = async event => {
 		event.preventDefault();
-		console.log(formState.inputs);
+
+		if (isLoginMode) {
+		} else {
+			try {
+				const response = await fetch('http://localhost:5000/api/users/signup', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						name: formState.inputs.name.value,
+						email: formState.inputs.email.value,
+						password: formState.inputs.password.value,
+					}),
+				});
+
+				const resposeData = await response.json();
+				console.log(resposeData);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+
 		auth.login();
 	};
 
